1321d1555abfa472bc81f0ee23a2cf88f566fa63
Paul Batchelor <pbatch@ccrma.stanford.edu>
2015-11-12 11:00:10 -0800

    removed all instances of DEBUGMODE

--- ugens/osc.c
+++ ugens/osc.c
@@ -19,7 +19,7 @@ int sporth_osc(sporth_stack *stack, void *ud)
     sp_osc *osc;
     switch(pd->mode){
         case PLUMBER_CREATE:
-#ifdef DEBUGMODE
+#ifdef DEBUG_MODE
             fprintf(stderr, "Creating osc function... \n");
 #endif
             sp_osc_create(&osc);
