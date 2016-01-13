35891962464a4ff7792bf81001c5ae535085c8a0
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-01-13 08:24:11 -0800

    added table modifiers to chorth

--- chorth/sporth.cpp
+++ chorth/sporth.cpp
@@ -15,9 +15,13 @@ CK_DLL_DTOR(sporth_dtor);
 CK_DLL_MFUN(sporth_setp);
 CK_DLL_MFUN(sporth_getp);
 
-CK_DLL_MFUN(sporth_parseString);
+CK_DLL_MFUN(sporth_get_table);
+CK_DLL_MFUN(sporth_set_table);
+
+CK_DLL_MFUN(sporth_parse_string);
 
 CK_DLL_TICK(sporth_get_tick);
+
 
 t_CKINT sporth_data_offset = 0;
 
@@ -45,7 +49,16 @@ CK_DLL_QUERY(Sporth)
     QUERY->add_mfun(QUERY, sporth_getp, "float", "pget");
     QUERY->add_arg(QUERY, "int", "index");
     
-    QUERY->add_mfun(QUERY, sporth_parseString, "string", "parse");
+    QUERY->add_mfun(QUERY, sporth_set_table, "float", "tset");
+    QUERY->add_arg(QUERY, "int", "index");
+    QUERY->add_arg(QUERY, "float", "val");
+    QUERY->add_arg(QUERY, "string", "table");
+    
+    QUERY->add_mfun(QUERY, sporth_get_table, "float", "tget");
+    QUERY->add_arg(QUERY, "int", "index");
+    QUERY->add_arg(QUERY, "string", "table");
+    
+    QUERY->add_mfun(QUERY, sporth_parse_string, "string", "parse");
     QUERY->add_arg(QUERY, "string", "arg");
     
     sporth_data_offset = QUERY->add_mvar(QUERY, "int", "@sporth_data", false);
@@ -103,6 +116,75 @@ CK_DLL_MFUN(sporth_setp)
     RETURN->v_float = val;
 }
 
+CK_DLL_MFUN(sporth_set_table)
+{
+    sporthData * data = (sporthData *) OBJ_MEMBER_INT(SELF, sporth_data_offset);
+
+
+    int i = GET_NEXT_INT(ARGS);
+    float val = GET_NEXT_FLOAT(ARGS);
+    
+    Chuck_String * ckstring = GET_CK_STRING(ARGS);
+    const char * cstr = ckstring->str.c_str();
+    char *ftname= (char *)malloc(strlen(cstr) + 1);
+    ftname = strdup(cstr);
+
+    int err = 0;
+
+    sp_ftbl *ft;
+
+    if(plumber_ftmap_search(&data->pd, ftname, &ft) == PLUMBER_NOTOK) {
+        fprintf(stderr, "Could not find ftable %s", ftname);
+        err++;
+    }
+
+    if(i > ft->size) {
+        fprintf(stderr, "Stack overflow! %d exceeds table size %d\n", i, ft->size);
+        err++;
+    }
+
+    if(!err) {
+        ft->tbl[i] = val;
+    }
+
+    free(ftname);
+    RETURN->v_float = val;
+}
+
+CK_DLL_MFUN(sporth_get_table)
+{
+    sporthData * data = (sporthData *) OBJ_MEMBER_INT(SELF, sporth_data_offset);
+
+
+    int err = 0;
+    int i = GET_NEXT_INT(ARGS);
+
+    sp_ftbl *ft;
+    
+    Chuck_String * ckstring = GET_CK_STRING(ARGS);
+    const char * cstr = ckstring->str.c_str();
+    char *ftname= (char *)malloc(strlen(cstr) + 1);
+    ftname = strdup(cstr);
+
+    if(plumber_ftmap_search(&data->pd, ftname, &ft) == PLUMBER_NOTOK) {
+        fprintf(stderr, "Could not find ftable %s", ftname);
+        err++;
+    }
+
+    if(i > ft->size) {
+        fprintf(stderr, "Stack overflow! %d exceeds table size %d\n", i, ft->size);
+        err++; 
+    }
+
+    free(ftname);
+
+    if(!err) {
+        RETURN->v_float = ft->tbl[i];
+    } else {
+        RETURN->v_float = 0;
+    }
+}
+
 CK_DLL_MFUN(sporth_getp)
 {
     sporthData * data = (sporthData *) OBJ_MEMBER_INT(SELF, sporth_data_offset);
@@ -110,7 +192,7 @@ CK_DLL_MFUN(sporth_getp)
     RETURN->v_float = data->pd.p[i];
 }
 
-CK_DLL_MFUN(sporth_parseString)
+CK_DLL_MFUN(sporth_parse_string)
 {
     sporthData * data = (sporthData *) OBJ_MEMBER_INT(SELF, sporth_data_offset);
     Chuck_String * ckstring = GET_CK_STRING(ARGS);
