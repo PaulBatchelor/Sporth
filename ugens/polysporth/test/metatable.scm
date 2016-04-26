05c13ef9985c96d713ca5d1469219ea4f13bccee
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-04-25 17:59:46 -0700

    updated tick documentation, metatable.scm

--- ugens/polysporth/test/metatable.scm
+++ ugens/polysporth/test/metatable.scm
@@ -1,13 +1,22 @@
-(define sine "'tmp' 'in' 0 metatable_copy 0 'args' tget mtof 0.1 0 'tmp' osc 400 butlp")
+(define meta "'tmp' 'in' 0 metatable_copy 'sin' 'in' 1 metatable_copy")
+(define synth 
+"tick 0 pset 0 'args' tget 
+5 1 0 'sin' osc 
+0.5 * + mtof 
+0.1 0 'tmp' osc 
+1000 butlp 
+0 p 0.1 0.6 0.1 tenv * ")
 
-(ps-eval 0 sine)
-(ps-eval 1 sine)
-(ps-eval 2 sine)
+(ps-eval 0 meta)
+
+(ps-eval 1 synth)
+(ps-eval 2 synth)
+(ps-eval 3 synth)
 
 (ps-noteblock-begin)
-(ps-note 0 2 0 4 '(69.0))
-(ps-note 0 2 2 4 '(71.0))
-(ps-note 0 2 4 4 '(73.0))
+(ps-note 1 3 0 4 '(69.0))
+(ps-note 1 3 2 4 '(71.0))
+(ps-note 1 3 4 4 '(73.0))
 (ps-noteblock-end)
 
 (define none (lambda () ()))
--- ugens/polysporth/test/metatable.sp
+++ ugens/polysporth/test/metatable.sp
@@ -1,6 +1,8 @@
 "in" 4 metatable_create
-"sine" 4096 "0 1 4096 -1" gen_line
-"sine" "in" 0 metatable_add
+"saw" 4096 "0 1 4096 -1" gen_line
+"sine" 4096 gen_sine
+"saw" "in" 0 metatable_add
+"sine" "in" 1 metatable_add
 
 0
 (60 120 2 * / dmetro )
