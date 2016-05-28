02430c357442e4eb5fcef7eb33849e7ffb4c48c7
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-05-28 01:46:13 -0700

    added bohlen pierce and alpha scale plugins

---
+++ util/plugins/alpha.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include <soundpipe.h>
+#include <sporth.h>
+
+
+static int sporth_alpha(plumber_data *pd, sporth_stack *stack, void **ud)
+{
+    int step = 0;
+    SPFLOAT out = 0;
+    SPFLOAT base = 0;
+    switch(pd->mode){
+        case PLUMBER_CREATE:
+            if(sporth_check_args(stack, "ff") != SPORTH_OK) {
+                stack->error++;
+                fprintf(stderr,"Invalid arguments for alpha.\n");
+                return PLUMBER_NOTOK;
+            }
+            sporth_stack_pop_float(stack);
+            sporth_stack_pop_float(stack);
+            sporth_stack_push_float(stack, 0.0);
+            break;
+        case PLUMBER_INIT:
+            sporth_stack_pop_float(stack);
+            sporth_stack_pop_float(stack);
+            sporth_stack_push_float(stack, 0.0);
+            break;
+        case PLUMBER_COMPUTE:
+            base = sporth_stack_pop_float(stack);
+            step = floor(sporth_stack_pop_float(stack));
+            out = base * pow(1.5, step / 9.0);
+            sporth_stack_push_float(stack, out);
+            break;
+        case PLUMBER_DESTROY:
+            break;
+        default:
+            break;
+    }
+    return PLUMBER_OK;
+}
+
+plumber_dyn_func sporth_return_ugen()
+{
+    return sporth_alpha;
+}
