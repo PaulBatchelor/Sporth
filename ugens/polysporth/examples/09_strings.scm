2996aecfb6d71af178273e943e9cc36a0c0765d3
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-11-25 13:27:26 -0800

    updated 09_strings.scm

--- ugens/polysporth/examples/09_strings.scm
+++ ugens/polysporth/examples/09_strings.scm
@@ -1,7 +1,6 @@
 (define prop "++2(-+)")
 (ps-parse 0 "120")
 (ps-push-string 0 prop)
-;(ps-parse 0 "120 '++2(-+)' prop 0.001 0.1 0.3 tenvx 1000 0.5 sine *")
 (ps-parse 0 "prop 0.001 0.1 0.3 tenvx 1000 0.5 sine *")
 (ps-init-sporthlet 0)
 (ps-turnon 0)
